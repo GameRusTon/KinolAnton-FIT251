b75e44b0f5810fd35ca1d3e5d94d6566810d630f
AntonKinol <kinolanton@gmail.com>
2026-07-12 23:58:23 +0600

    Исправлено название тестов на SpaceshipTests. Исправлены реализации интерфесов, методы выполнены более корректно.

--- task04/Fighter.cs
+++ task04/Fighter.cs
@@ -6,16 +6,37 @@ public class Fighter : ISpaceship
 
     public int FirePower => 50;
 
+    public double X {get; private set;}
+
+    public double Y {get; private set;}
+    
+    public int DirAngle {get; private set;}
+
+    public int Ammo {get; private set;}
+
+    public Fighter (double x = 0, double y = 0, int StartAmmo = 10)
+    {
+        X = x;
+        Y = y;
+        DirAngle = 0;
+        Ammo = StartAmmo;
+    }
+
     public void MoveForward()
     {
-        
+        double rad = DirAngle * (Math.PI / 180.0);
+
+        X += Speed * Math.Cos(rad);
+        Y += Speed * Math.Sin(rad);
     }
     public void Rotate(int angle)
     {
-        
+        DirAngle = (DirAngle + angle) % 360;
+        if (DirAngle < 0) DirAngle += 360;
     }
     public void Fire()
     {
-        
+        if (Ammo > 0)
+        Ammo--;
     }
 }
