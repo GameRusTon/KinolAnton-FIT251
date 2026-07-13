3470f4cd294f00d285f954923379bb037bc0692b
AntonKinol <kinolanton@gmail.com>
2026-07-13 18:13:48 +0600

    Сделано 14 задание 3 недели практики. Реализовано вычисление определенного интеграла методом трапеции в несколько потоков. Написан класс DefiniteIntegral с разбиением отрезка по потокам, использован Barrier для синхронизации и Interlocked для суммирования. Написаны тесты для проверки, ошибок не возникает.

---
+++ task14/DefiniteIntegral.cs
@@ -0,0 +1,91 @@
+﻿namespace task14;
+
+public class DefiniteIntegral
+{
+    public static double Solve(double a, double b, Func<double, double> function, double step, int threadsnumber)
+    {
+        if (function == null)
+        throw new ArgumentNullException("Функция не должна быть пустой.");
+
+        if (step <= 0)
+        throw new ArgumentException("Шаг вычислений должен быть положителен.");
+
+        if (threadsnumber <= 0)
+        throw new ArgumentException("Количество потоков не может быть отрицательным.");
+
+        double Width = (b - a) / threadsnumber;
+
+        Thread[] threads = new Thread[threadsnumber];
+
+        double result = 0.0;
+        
+        using (Barrier barrier = new Barrier(participantCount: threadsnumber + 1))
+        {
+            for (int i = 0; i < threadsnumber; i++)
+            {
+                double start = a + i * Width;
+                double end = (i + 1 != threadsnumber) ? start + Width : b;
+
+                double localStart = start;
+                double localEnd = end;
+
+                threads[i] = new Thread(() =>
+                {
+                    double ThreadResult = SegmentCalculation(localStart, localEnd, step, function);
+
+                    InterlockedSum(ref result, ThreadResult);
+
+                    barrier.SignalAndWait();
+                });
+
+                threads[i].Start();
+            }
+
+            barrier.SignalAndWait();
+        }
+
+        return result;
+    }
+
+    //Подсчет результатов в потоках.
+    private static double SegmentCalculation(double start, double end, double step, Func<double, double> function)
+    {
+        double ThreadResult = 0.0;
+
+        double StepStart_X = start;
+        
+        while (StepStart_X < end)
+        {
+            double StepEnd_X = StepStart_X + step;
+            if (StepEnd_X > end)
+            StepEnd_X = end;
+
+            double StepStart_Y = function(StepStart_X);
+
+            double StepEnd_Y = function(StepEnd_X);
+
+            ThreadResult += (StepStart_Y + StepEnd_Y)/2 * (StepEnd_X - StepStart_X);
+
+            StepStart_X = StepEnd_X;
+        }
+        
+        return ThreadResult;
+    }
+
+    //Метод для безопасного суммирования результатов с использованием Interlocked операции.
+    private static void InterlockedSum(ref double result, double ThreadResult)
+    {
+        double currentResult = result;
+
+        while(true)
+        {
+            double modifiedResult = currentResult;
+            double ThreadResultPlus = modifiedResult + ThreadResult;
+
+            currentResult = Interlocked.CompareExchange(ref result, ThreadResultPlus, modifiedResult);
+
+            if (modifiedResult == currentResult)
+            break;
+        }
+    }
+}
---
+++ task14tests/DefiniteIntegralTests.cs
@@ -0,0 +1,42 @@
+using task14;
+namespace task14tests;
+
+public class DefiniteIntegralTests
+{
+    [Fact]
+    public void Solve_WhenEverythingIsCorrect()
+    {
+        Func <double, double> X = (double x) => x;
+        Func <double, double> SIN = (double x) => Math.Sin(x);
+
+        Assert.Equal(0, DefiniteIntegral.Solve(-1, 1, X, 1e-4, 2), 1e-4);
+
+        Assert.Equal(0, DefiniteIntegral.Solve(-1, 1, SIN, 1e-5, 8), 1e-4);
+
+        //В данном тесте в задании изначально была ошибка в результате. Была исправлена.
+        Assert.Equal(12.5, DefiniteIntegral.Solve(0, 5, X, 1e-6, 8), 1e-5);
+    }
+
+    [Fact]
+    public void Solve_WhenFunctionIsNull_ThrowsArgumentException()
+    {
+        Func <double, double> X = (double x) => x;
+        Assert.Throws<ArgumentNullException>(() => DefiniteIntegral.Solve(-1, 1, null!, 1e-4, 2));
+    }
+
+    [Fact]
+    public void Solve_WhenStepIsZeroOrNegative_ThrowsArgumentException()
+    {
+        Func <double, double> X = (double x) => x;
+        Assert.Throws<ArgumentException>(() => DefiniteIntegral.Solve(-1, 1, X, 0, 2));
+        Assert.Throws<ArgumentException>(() => DefiniteIntegral.Solve(-1, 1, X, -1, 2));
+    }
+
+    [Fact]
+    public void Solve_WhenThreadsNumberIsZeroOrNegative_ThrowsArgumentException()
+    {
+        Func <double, double> X = (double x) => x;
+        Assert.Throws<ArgumentException>(() => DefiniteIntegral.Solve(-1, 1, X, 1e-4, 0));
+        Assert.Throws<ArgumentException>(() => DefiniteIntegral.Solve(-1, 1, X, 1e-4, -1));
+    }
+}
